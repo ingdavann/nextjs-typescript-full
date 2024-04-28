5f3c69d1a37c069d6a2e7116512c9c9a602f3185
ingdavann <ingdavann4444@gmail.com>
2024-04-28 21:57:01 +0700

    Added: Server fetching

---
+++ app/(user)/service/[id]/page.tsx
@@ -0,0 +1,27 @@
+import CardProductDetail from "@/components/card/cardProductDetail";
+
+type PropsParams = {
+    params: {
+        id: number
+    };
+    searchParam: any
+}
+const ENDPOINT = "https://fakestoreapi.com/products/";
+export const getData = async (id: number) => {
+  const res = await fetch(`${ENDPOINT}${id}`);
+  const data = await res.json();
+  return data;
+}  
+
+export default async function Detail(props: PropsParams) {
+  const data = await getData(props.params.id);
+  return (
+    <div className='h-screen grid place-content-center'>
+      <CardProductDetail 
+        title={data.title}
+        image={data.image || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8oruMfmenl0Gf8qmyupis5g9F-YyXyh9K4xunOWJJtg&s"}
+        price={data.price}
+      />
+    </div>
+  )
+}
