301cf5d920b43ade5af14a40f74c7215006cbaf5
ingdavann <ingdavann4444@gmail.com>
2024-04-29 13:51:39 +0700

    Added: dynamic metadata in product detail

--- app/(user)/service/[id]/page.tsx
+++ app/(user)/service/[id]/page.tsx
@@ -1,19 +1,41 @@
 import CardProductDetail from "@/components/card/CardProductDetail";
+import { Metadata, ResolvingMetadata } from "next";
 
-type PropsParams = {
-    params: {
-        id: number 
-    };
-    // searchParam: any
+type Props = {
+    params: {id: string};
+    searchParams: {[key: string]: string | string[] | undefined};
 }
+
 const ENDPOINT = "https://fakestoreapi.com/products/";
-const getData = async (id: number) => {
+
+const getData = async (id: string) => {
   const res = await fetch(`${ENDPOINT}${id}`);
   const data = await res.json();
   return data;
 }  
 
-export default async function Detail(props: PropsParams) {
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  // read route params
+  const id = params.id
+ 
+  // fetch data
+  const product = await fetch(`https://fakestoreapi.com/products/${id}`).then((res) => res.json())
+ 
+  // optionally access and extend (rather than replace) parent metadata
+  // const previousImages = (await parent).openGraph?.images || []
+ 
+  return {
+    title: product.title,
+    openGraph: {
+      images: product.images
+    },
+  }
+}
+
+export default async function Detail(props: Props) {
   const data = await getData(props.params.id);
   return (
     <div className='h-screen grid place-content-center'>
